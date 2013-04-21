0a42cc943255379dc3be9c23e168dc3f964ac388
Florent Biville <f.biville@lateral-thoughts.com>
2013-04-21 06:26:51 +0200

    Exercise 2 solution.

--- src/main/webapp/resources/scripts/modules/fixMe.js
+++ src/main/webapp/resources/scripts/modules/fixMe.js
@@ -1,14 +1,19 @@
+/*global alert:true,
+ console:true,
+ superNeededGlobal:true*/
 /**
  * I'm a dummy module, but JSLint does not like me
  */
 require(['jquery'], function($) {
     return {
         doSomething: function() {
-            var a = 2;
-            var b = 3;
+            var a = 2,
+                b = 3;
             console.log(a + b);
-            if (a == b) alert("OMG!");
+            if (a === b) {
+                alert("OMG!");
+            }
             superNeededGlobal = "I need to be exported to the root scope";
         }
-    }
+    };
 });
